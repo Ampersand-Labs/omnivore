8c83057d924f8f856f7e3c7ae9a9f6ca8138a7ae
Yosep Kim <yosepk@gmail.com>
2015-07-27 23:06:32 -0400

    added test for client

--- spec/cases/client_spec.rb
+++ spec/cases/client_spec.rb
@@ -2,19 +2,13 @@ require 'spec_helper'
 
 describe "OmnivoreApi::Client" do
   it "has an attr_reader for access token" do
-    consumer_key = 'adfadf'
-    service = OmnivoreApi::Client.new :sandbox, consumer_key, double
-    expect(service.consumer_key).to eq(consumer_key)
-  end
-
-  it "has an attr_reader for app_secret" do
-    consumer_secret = 'double'
-    service = OmnivoreApi::Client.new :sandbox, double, consumer_secret
-    expect(service.consumer_secret).to eq(consumer_secret)
+    api_key = 'adfadf'
+    service = OmnivoreApi::Client.new api_key
+    expect(service.api_key).to eq(api_key)
   end
 
   it "has an attr_reader for server" do
-    service = OmnivoreApi::Client.new :sandbox, double, double
-    expect(service.server).to eq('https://omnivore-test.apigee.net')
+    service = OmnivoreApi::Client.new double
+    expect(service.server).to eq('https://api.omnivore.io')
   end
 end
